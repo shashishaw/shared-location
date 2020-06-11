75a346a59470b677e8437344c9d6c9b37877545a
Shashikant Shaw <shasikant.shaw@railyatri.in>
2020-06-11 22:44:52 +0530

    some changes in migration

--- db/migrate/20200609183353_add_name_to_users.rb
+++ db/migrate/20200609183353_add_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameToUsers < ActiveRecord::Migration[6.0]
   def change
   	 add_column :users, :name, :string
-  	 add_column :users, :mobile_no, :integer,  :limit => 11
+  	 add_column :users, :mobile_no, :integer
   end
 end
--- db/migrate/20200609190302_change_integer_size_to_users.rb
+++ db/migrate/20200609190302_change_integer_size_to_users.rb
@@ -1,5 +1,5 @@
 class ChangeIntegerSizeToUsers < ActiveRecord::Migration[6.0]
   def change
-  	 change_column :users, :mobile_no, :string, :limit => 11
+  	 change_column :users, :mobile_no, :string
   end
 end
